b2d87003ff477e485d3044d8d351e053efe9c2e4
Vindelev <vindelev0907@live.com>
2018-11-14 18:14:10 +0100

    Updated database

--- Database/Data/Entities/Person.cs
+++ Database/Data/Entities/Person.cs
@@ -5,9 +5,13 @@ namespace Database.Data.Entities
     public class Person
     {
 
+        public int Id { get; set; }
+
         [StringLength(30, MinimumLength = 2)]
         public string Name { get; set; }
 
         public int Age { get; set; }
+
+        public string CPR { get; set; }
     }
 }
