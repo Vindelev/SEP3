847b67692557c6b623025ae0e6b7f61b53d04c2d
Mihail Kanchev <colty.gaming@gmail.com>
2018-11-18 09:04:35 +0100

    Minor middleware changes

--- Middleware/src/test.java
+++ Middleware/src/test.java
@@ -3,6 +3,11 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientProperties;
+
+import java.lang.Thread;
 
 public class test
 {
@@ -10,16 +15,14 @@ public class test
    public static void main(String[] args) throws InterruptedException
    {
       Client client = ClientBuilder.newClient();
-      WebTarget target = client.target("http://localhost:5000/api/people/6969696969");
-      try {
-         System.out.println(target.request(MediaType.TEXT_PLAIN).get());
-      }
-      catch(RedirectionException e) {
-         while(true) {
-            System.out.println("Waiting for an answer");
-            Thread.sleep(1000);
-         }
-      }
+     
+      WebTarget target = client.target("http://localhost:5000/api/people");
+      target.property(ClientProperties.FOLLOW_REDIRECTS, Boolean.TRUE);
+      
+      Response response = target.request().get();
+      response.bufferEntity();
+      System.out.println(response.readEntity(String.class));
+    
       
    }
 
